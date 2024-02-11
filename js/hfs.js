cac495a0cf840f4651c31cec3619689277787005
vishvjeet79 <vishvjeet180@gmail.com>
2024-02-12 00:10:44 +0530

    header footer links refed to root

--- js/hfs.js
+++ js/hfs.js
@@ -1,5 +1,5 @@
 //for loading header and footer in every page using jQuery
-$(function(){
+$(function () {
     $("#header").load("/header.html");
     $("#footer").load("/footer.html");
 })
@@ -7,11 +7,11 @@ $(function(){
 //for enabling searchbar to appear onClick search button
 //onClick() not working on injected HTML, thus, using .on event handler
 function cardmargin() {
-    
-        $(".togglesearch").toggle();
-        $("input[type='text']").focus();
-    }
-    $(document).on('click','.sbtn', cardmargin);
+
+    $(".togglesearch").toggle();
+    $("input[type='text']").focus();
+}
+$(document).on('click', '.sbtn', cardmargin);
 
 
 
